8cf31a18780e6b1038f60ad7af700b9090fea794
Pedro Leite <pedrohleite2005@gmail.com>
2025-08-21 17:48:52 -0300

    feat: criação e configuração pacotes main e router

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"devbook-app/pkg/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	app := router.Gerar()
+
+	fmt.Println("Rodando WebApp!")
+	log.Fatal(http.ListenAndServe(":3000", app))
+}
