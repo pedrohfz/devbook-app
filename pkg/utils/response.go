7a2ae0f8860459b6ff7c232e78cbcfb2df39202f
Pedro Leite <pedrohleite2005@gmail.com>
2025-09-23 18:29:33 -0300

    chore: lib sweetalert

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -16,7 +16,7 @@ func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if dados != nil {
+	if statusCode != http.StatusNoContent {
 		if err := json.NewEncoder(w).Encode(dados); err != nil {
 			log.Fatal(err)
 		}
